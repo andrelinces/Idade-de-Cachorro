e6ee044f639d458bc672006cf462054802df25ea
Andre Linces <andrelinces@yahoo.com.br>
2021-08-13 22:56:01 -0300

    Realizada a primeira instrução de código. Utilizando control e arrastando para área de código. Configurando a ação do botão para exibir o resultado.

--- Idade de Cachorro/ViewController.swift
+++ Idade de Cachorro/ViewController.swift
@@ -8,6 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var legendaResultado: UILabel!
+    
+    @IBAction func descobrirIdade(_ sender: Any) {
+        
+        legendaResultado.text = "Texto alterado"
+        
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
